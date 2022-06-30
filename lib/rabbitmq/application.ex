71f8539e32f1424e4a90918af1b9708febad59e1
Leonardo Avanzi <47260167+leonardoavanzi@users.noreply.github.com>
2022-06-30 20:22:17 -0300

    initial commit

---
+++ lib/rabbitmq/application.ex
@@ -0,0 +1,20 @@
+defmodule Rabbitmq.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Rabbitmq.Worker.start_link(arg)
+       {Rabbitmq.WorkerSendMessage, nil}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Rabbitmq.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ lib/rabbitmq/worker_send_message.ex
@@ -0,0 +1,35 @@
+defmodule Rabbitmq.WorkerSendMessage do
+  use GenServer
+
+  def init(_) do
+    {:ok, nil}
+  end
+
+  def start_link(_) do
+    IO.puts("worker send message started !")
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def start() do
+    name = IO.gets("Name: ")
+    send_message(name)
+  end
+
+  def send_message(name) do
+    # open pid connection
+    {:ok, pid} = AMQP.Connection.open()
+
+    # channel open
+    {:ok, channel} = AMQP.Channel.open(pid)
+
+    message = IO.gets("your input here: ")
+    text = "#{name}" <> message
+
+    AMQP.Exchange.declare(channel, "MARMECQMARIAMEC", :direct)
+
+    AMQP.Basic.publish(channel, "MARMECQMARIAMEC", "message", text)
+    AMQP.Connection.close(pid)
+
+
+  end
+end
